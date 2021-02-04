77160d99643d51adf31b07e66b66d509fbab28ad
Daniil Shaposhnikov <daniil.shaposhnikov@gmail.com>
2021-02-04 23:58:47 +0500

    added basic editor matrix ciew generator + rect helper

---
+++ Assets/Prototype/Scripts/Utils/Editor/GridCreatorEditor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UniRx;
+using UnityEditor;
+using UnityEngine;
+
+namespace Prototype.Scripts.Utils.Editor
+{
+    [CustomEditor(typeof(GridCreator))]
+    public class GridCreatorEditor : UnityEditor.Editor
+    {
+        private bool currentToggle = false;
+
+        public override void OnInspectorGUI()
+        {
+            var gridCreator = target as GridCreator;
+            
+            
+            base.OnInspectorGUI();
+            
+            var clearButtonPressed = GUILayout.Button("Clear");
+            if (clearButtonPressed)
+                gridCreator.ClearObjectChildren();
+            var applyButtonPressed = GUILayout.Button("Apply");
+            if (applyButtonPressed || currentToggle)
+                gridCreator.ApplyGridCreatorModifications();
+            
+            currentToggle = GUILayout.Toggle(currentToggle, "Apply in Update?");
+        }
+
+
+    }
+}
--- Assets/Prototype/Scripts/Utils/RectTransformHelper.cs
+++ Assets/Prototype/Scripts/Utils/RectTransformHelper.cs
@@ -2,8 +2,21 @@
 
 namespace Prototype.Scripts.Utils
 {
-    public static class RectTransformUtils
+    public static class RectTransformHelper
     {
+        public static Vector2 GetGridContainer(Rect childRect, int rows, int columns, float childrenOffset)
+        {
+            return new Vector2(rows * childRect.width + (rows + 1) * childrenOffset,
+                columns * childRect.height + (columns + 1) * childrenOffset);
+        }
+
+        public static Vector2 GetChildPsotionContainer(Rect childRect, int xIndex, int yIndex, float childrenOffset)
+        {
+            Vector2 resultPosition = Vector2.zero;
+            resultPosition.x = childrenOffset * (xIndex + 1) + childRect.width * xIndex;
+            resultPosition.y = childrenOffset * (yIndex + 1) + childRect.height * yIndex;
+            return resultPosition;
+        }
         
     }
 }
