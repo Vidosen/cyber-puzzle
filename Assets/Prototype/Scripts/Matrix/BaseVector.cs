7922f40cbd1fde0fea350454a48d10a860d8da45
Daniil Shaposhnikov <vidosen@yandex.ru>
2021-06-06 20:25:49 +0500

    Moving Canvas testing & changes

--- Assets/Prototype/Scripts/Matrix/BaseVector.cs
+++ Assets/Prototype/Scripts/Matrix/BaseVector.cs
@@ -6,30 +6,34 @@ using UnityEngine.EventSystems;
 
 namespace Prototype.Scripts.Matrix
 {
-    public abstract class BaseVector : MonoBehaviour, IDragHandler, IEndDragHandler, IBeginDragHandler, IDisposable {
-
+    public abstract class BaseVector : MonoBehaviour, IDragHandler, IEndDragHandler, IBeginDragHandler, IDisposable
+    {
+        
         public int Size => cells?.Length ?? 0;
         public MatrixCell[] Cells => cells;
         public RectTransform ThisTransform =>
             _thisTransform == null ? _thisTransform = transform as RectTransform : _thisTransform;
         public bool IsDragging => _isDragging;
+        
 
         public bool IsInitialized => cells != null;
         public abstract Vector2 SnapDirection { get; protected set; }
 
         private MatrixCell[] cells;
-        
+        [SerializeField] private float _HighlightZOffset = 30;
         [SerializeField] private TextMeshProUGUI LineIndexText;
         [SerializeField, Range(0, 1)] private float DragAlpha = 1f;
         private RectTransform _thisTransform;
         private Canvas _canvas;
         private CanvasGroup _canvasGroup;
         private bool _isDragging;
+        private float _deafultZ;
 
         private void Awake()
         {
             _canvas = GetComponentInParent<Canvas>();
             _canvasGroup = GetComponentInParent<CanvasGroup>();
+            _deafultZ = ThisTransform.localPosition.z;
         }
 
         private void OnDestroy()
@@ -83,14 +87,17 @@ namespace Prototype.Scripts.Matrix
         public void OnEndDrag(PointerEventData eventData)
         {
             foreach (var cell in cells)
+            {
                 cell.UnpinVector();
+                cell.SetLocalZ(_deafultZ);
+            }
             
             _isDragging = false;
             _canvasGroup.blocksRaycasts = !_isDragging;
             _canvasGroup.alpha = 1f;
             
-            _canvas.sortingOrder = 10;
-            
+            SetSortingOrder(10);
+            SetLocalZ(_deafultZ);
             ThisTransform.anchoredPosition = Vector3.zero;
             Debug.Log($"{GetType().Name}.OnEndDrag");
         }
@@ -101,10 +108,13 @@ namespace Prototype.Scripts.Matrix
             _canvasGroup.blocksRaycasts = !_isDragging;
             _canvasGroup.alpha = DragAlpha;
 
-            _canvas.sortingOrder = 100;
-            
+            SetSortingOrder(100);
+            SetLocalZ(_deafultZ - _HighlightZOffset);
             foreach (var cell in cells)
+            {
                 cell.PinVector(ThisTransform);
+                cell.SetLocalZ(_deafultZ - _HighlightZOffset);
+            }
         }
 
         public BaseVector Initialize(int cellsCount, int index)
@@ -128,5 +138,14 @@ namespace Prototype.Scripts.Matrix
                 Array.ForEach(cells, c => c.Dispose());
             Destroy(gameObject);
         }
+
+        private void SetLocalZ(float newZ)
+        {
+            var newPosition = ThisTransform.localPosition;
+            newPosition.z = newZ;
+            ThisTransform.localPosition = newPosition;
+        }
+
+        private void SetSortingOrder(int order) => _canvas.sortingOrder = order;
     }
 }
