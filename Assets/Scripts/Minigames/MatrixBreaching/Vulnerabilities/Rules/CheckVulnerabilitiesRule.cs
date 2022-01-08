7d3922d7cf081757101ba7a018d01fecabd70957
Daniil Shaposhnikov <vidosen@yandex.ru>
2022-01-08 14:48:49 +0500

    Added more complicated vulnerability creation

--- Assets/Scripts/Minigames/MatrixBreaching/Vulnerabilities/Rules/CheckVulnerabilitiesRule.cs
+++ Assets/Scripts/Minigames/MatrixBreaching/Vulnerabilities/Rules/CheckVulnerabilitiesRule.cs
@@ -1,35 +1,58 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Minigames.MatrixBreaching.Matrix.Models;
 using Minigames.MatrixBreaching.Matrix.Signals;
+using Minigames.MatrixBreaching.Vulnerabilities.Models;
 using Minigames.MatrixBreaching.Vulnerabilities.Services;
+using UniRx;
 using Zenject;
 
 namespace Minigames.MatrixBreaching.Vulnerabilities.Rules
 {
-    public class CheckVulnerabilitiesRule : IInitializable
+    public class CheckVulnerabilitiesRule : IInitializable, IDisposable
     {
-        private SignalBus _signalBus;
-        private VulnerabiltyInventory _vulnerabiltyInventory;
+        private readonly SignalBus _signalBus;
+        private readonly VulnerabiltyInventory _vulnerabiltyInventory;
         private readonly VulnerabilityService _vulnerabilityService;
+        private readonly CompositeDisposable _compositeDisposable = new CompositeDisposable();
 
-        public CheckVulnerabilitiesRule(SignalBus signalBus, VulnerabiltyInventory vulnerabiltyInventory, VulnerabilityService vulnerabilityService)
+        public CheckVulnerabilitiesRule(SignalBus signalBus, VulnerabiltyInventory vulnerabiltyInventory,
+            VulnerabilityService vulnerabilityService)
         {
             _vulnerabiltyInventory = vulnerabiltyInventory;
             _vulnerabilityService = vulnerabilityService;
             _signalBus = signalBus;
         }
+
         public void Initialize()
         {
-            _signalBus.Subscribe<MatrixOperationsSignals.IOperationSignal>(CheckVulnerabilities);
+            _signalBus.GetStream<MatrixOperationsSignals.IOperationSignal>().Subscribe(_ => CheckVulnerabilities())
+                .AddTo(_compositeDisposable);
+            _vulnerabiltyInventory.ModelAdded.Subscribe(addEvent => CheckVulnerabilities())
+                .AddTo(_compositeDisposable);
+        }
+
+        public void Dispose()
+        {
+            _compositeDisposable.Clear();
         }
 
         private void CheckVulnerabilities()
         {
             foreach (var model in _vulnerabiltyInventory)
-            {
-                IList<ValueCell> foundMatchedMatrixCells = _vulnerabilityService.FindBestMatchForVulnerability(model);
-                model.SetMatchedMatrixSequence(foundMatchedMatrixCells);
-            }
+                CheckVulnerability(model);
+        }
+
+        private void CheckVulnerability(VulnerabilityModel model)
+        {
+            IList<ValueCell> foundMatchedMatrixCells;
+            if (model.IsInitialized)
+                foundMatchedMatrixCells =
+                    _vulnerabilityService.FindBestMatchForVulnerabilitySequence(model.VulnerabilitySequence);
+            else
+                foundMatchedMatrixCells = new List<ValueCell>();
+
+            model.SetMatchedMatrixSequence(foundMatchedMatrixCells);
         }
     }
 }
